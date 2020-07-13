924a661de329e71dd08e7f9f10eabf18a281672d
BIKASH-NONA <qwertyattherate17@gmail.com>
2020-07-13 19:39:55 +0530

    added THE INTRO.txt,add.cpp and add.exe

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main()
+{int a,b;
+ cout<<"ENTER AN INTEGER:";
+ cin>>a;
+ cout<<"ENTER ANOTHER INTEGER:";
+ cin>>b;
+ cout<<"THE SUM OF TWO INTEGERS:"<<a+b;
+}
